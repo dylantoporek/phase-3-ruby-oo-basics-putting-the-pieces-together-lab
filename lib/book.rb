5a97026566aef4da3c07c5eb6afa014093a4ed51
DylanToporek <dylantoporek@gmail.com>
2021-12-08 14:55:16 -0500

    commit tree 35dbfb1884e8c225e481002ab93b3cd8d8ffbeac

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,23 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    def initialize(title)
+        @title = title
+    end
 
+    def author_name(author)
+        @author = author
+    end
+
+    def pages(page_count)
+        @page_count = page_count
+    end
+
+    def type(genre)
+        @genre = genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,27 @@
 # Make your shoe class here!
+
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+    def initialize(brand)
+        @brand = brand
+    end
+    def style(color)
+        @color = color
+    end
+    def fit(size)
+        @size = size
+    end
+
+    def make(material)
+        @material = material
+    end
+
+    def upkeep(condition)
+        @condition = condition
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
